942427d70efada716cea8bc8dfc46f06402e37b2
sooooooongyi <sooooooongyi@gmail.com>
2024-10-17 17:05:43 +0900

    feat: IoC Container & Bean

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,14 +4,18 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class MemoService {
     private final MemoRepository memoRepository;
 
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
+    }
 
     public MemoResponseDto createMemo(MemoRequestDto requestDto) {
         Memo memo = new Memo(requestDto);
